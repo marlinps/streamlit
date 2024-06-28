719d4408a327c3350717249a8e1899ffc7cc6ec5
Marlin Purnama Sari <marlinpurnamas92@gmail.com>
2024-06-28 17:22:10 +0700

    solved issue

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,36 +1,6 @@
-import altair as alt
 import streamlit as st 
 import pandas as pd 
 from st_aggrid import AgGrid
-
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
-
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
-
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
-
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
-tab1, tab2 = st.tabs(["Tab 1", "Tab2"])
-tab1.write("this is tab 1")
-tab2.write("this is tab 2")
 
 house = pd.read_csv('house_clean.csv', usecols=['id','price'])
 
